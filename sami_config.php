f7a342652f9d0d8aecc0c5dfd046104618c39149
Riikka Kalliomäki <riikka.kalliomaki@gmail.com>
2018-07-19 14:08:30 +0300

    Switch to centralized configs

--- sami_config.php
+++ sami_config.php
@@ -1,26 +1,3 @@
 <?php
 
-use Sami\RemoteRepository\GitHubRemoteRepository;
-use Sami\Sami;
-use Symfony\Component\Finder\Finder;
-
-$iterator = Finder::create()
-    ->files()
-    ->name('*.php')
-    ->in(__DIR__ . '/src');
-
-$theme = getenv('SAMI_THEME');
-$settings = [];
-
-if ($theme) {
-    $settings['theme'] = basename($theme);
-    $settings['template_dirs'] = [dirname($theme)];
-}
-
-return new Sami($iterator, $settings + [
-    'title'                => 'Web Application Request Router API',
-    'build_dir'            => __DIR__ . '/build/doc',
-    'cache_dir'            => __DIR__ . '/build/cache',
-    'remote_repository'    => new GitHubRemoteRepository('simply-framework/router', __DIR__),
-    'default_opened_level' => 2,
-]);
+return require __DIR__ . '/vendor/riimu/sami-config/config.php';
